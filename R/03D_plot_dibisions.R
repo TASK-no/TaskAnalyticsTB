613e931eb556004a5894db4dc90363424505b208
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-03-23 10:50:15 +0100

    Update data digitial competence plot
    - use two types of divions data:
        - type 1: divisions_area: fixed digital competence level,
                  results are mapped to expertise area distribution
        - type 2: divisions_comptenence: fixed expertise area,
                  results are mapped to digital competence distribution
    
    -> allow plotting the same plot-type (arguments for title/axis etc.
    are meant to pass via a shiny module or manually in a script analysis)

--- R/03D_plot_dibisions.R
+++ R/03D_plot_dibisions.R
@@ -1,10 +1,14 @@
 #' Generate radar-type plot
 #'
 #' @inheritParams plot_pie_figures
+#' @param title title of plot
+#' @param y_title sub-title of y-axis names (which consist of year and
+#'   `y_title`)
 #'
 #' @return a ggplot2-object of the radar plot type
 #' @export
 plot_division <- function(data_set, year,
+                          title, y_title = "Digital kompetanse i divisjonene",
                           col_scm = c(light_blue = "#189BC4",
                                       dark_blue = "#3F505A",
                                       orange = "#F59331",
@@ -13,28 +17,32 @@ plot_division <- function(data_set, year,
                                       dark_green = "#356A1A",
                                       black1 = "#1F282D",
                                       black2 = "#263036")) {
-  return(plot_division_shy(data_set, year = year, col_scm))
+  return(plot_division_shy(data_set, year = year,
+                           title = paste0("\u00c5", "r ", year, " - ", title),
+                           y_title = y_title, col_scm))
 }
-plot_division_shy <- function(df, year, col_scm) {
-  browser()
+plot_division_shy <- function(df, year, title, y_title, col_scm) {
   col_scm_used <- unname(col_scm[1:4])
 
   fig <- plotly::plot_ly(data = df)
-  fig <- fig %>%
+  fig %>%
     plotly::add_trace(y = ~SamDivision,
                       x = ~perc,
                       color = ~sub_category,
                       colors = col_scm_used,
                       type = "bar",
-                      text = ~total_num,
-                      hoverinfo = 'text') %>%
+                      text = ~paste(perc, "%"),
+                      textposition = "inside",
+                      insidetextfont = list(color = "white"),
+                      hovertext = ~total_num,
+                      hoverinfo = "text") %>%
     plotly::layout(legend = list(y = -0.05,
                                  orientation = "h",
                                  x = 0),
-                   title = list(text = paste0("\u00c5", "r ", year)),
-                   yaxis = list(title = list(text = "Digital kompetanse per divisjon",
+                   title = list(text = title),
+                   yaxis = list(title = list(text = y_title,
                                              standoff = 7,
-                                             font = list(size = 24))),
+                                             font = list(size = 14))),
                    xaxis = list(title = list(text = "")),
                    barmode = "stack")
 }
