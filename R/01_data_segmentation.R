53672194b49b1befc85930cf8a6c12bb282a8d2f
IlyaZar <zarubin@wiso.uni-koeln.de>
2024-02-23 11:17:02 +0100

    docs: add docstrings for segmentation_analysis() function

--- R/01_data_segmentation.R
+++ R/01_data_segmentation.R
@@ -1,8 +1,28 @@
 #' Perform a segmentation analysis on the raw data
 #'
-#' @param data_SVV raw data (as a \code{data.frame})
+#' This function applies a comprehensive segmentation analysis across multiple
+#' questionnaire items (Q16, Q17, Q14, Q19), utilizing specific settings for
+#' each to categorize responses into defined skill levels: "Uerfaren",
+#' "Grunnleggende", "Videregående", and "Avansert". The analysis involves
+#' recoding responses, aggregating scores, and classifying these scores into the
+#' aforementioned categories based on predefined criteria in the settings.
 #'
-#' @return data set after segmentation analysis
+#' @param data_SVV raw data (as a \code{data.frame})
+#' @param ind1_vals numeric vector giving which values to recode from
+#' @param settings_q16 Settings for Q16 - Communication and interaction,
+#'   encapsulated in a list with keys: `sum_score_val_grun`,
+#'   `sum_score_val_vide`, `sum_score_val_avan` (numeric values setting
+#'   thresholds for each skill level), and `type_val_grun`, `type_val_vide`,
+#'   `type_val_avan` (strings specifying the comparison type, either
+#'   "larger-equal" or "equal").
+#' @param settings_q17 Similar to `settings_q16` but for Q17 - Information
+#'   security and privacy.
+#' @param settings_q14 Similar to `settings_q16` but for Q14 - Use of software.
+#' @param settings_q19 Similar to `settings_q16` but for Q19 - Use of
+#'   technology.
+#' @return A \code{data.frame} enriched with segmentation variables that
+#'   indicate the skill level classifications for each of the questionnaire
+#'   items analyzed.
 #' @export
 segmentation_analysis <- function(data_SVV,
                                   ind1_vals = c(3, 4),
